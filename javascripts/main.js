ea70ebf6c0cd53b48edbd66acc20c8da68c5b78e
Tryggvi Bjorgvinsson <tryggvi.bjorgvinsson@okfn.org>
2013-04-15 21:54:19 +0000

    Modified the javascript to reload the site with url parameters that define where we've drilled until now. This creates a unique url for each drilldown.
    Can be improved (e.g. the alert when no children are present or let user know where he/she is at without having to 'parse' it from the url parameters.
    
    This introduces a dependency on purl.js (jQuery based url parser). It could be implemented easily but why go through the extra work?

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,16 +1,48 @@
 $(function() {
+    // Default drilldowns and cuts
+    var drilldowns = ["department", "unit", "child-fund"];
+    var cuts = {"time.year": "2011|time.year:2012"};
+
+    // Get url parameters (this could be easily parsed, but we use purl)
+    // Purl is available here: https://github.com/allmarkedup/jQuery-URL-Parser
+    var parameters = $.url().param();
+    
+    // While the first drilldown is in the url parameters
+    // we move it to the cuts instead
+    while(drilldowns[0] in parameters) {
+	var drill = drilldowns.shift();
+	cuts[drill] = parameters[drill];
+    }
+
+    // Create the state from the (possibly modified) drilldowns and cuts
     var state = {
-	"drilldowns": [
-	    "department",
-	    "unit",
-	    "child-fund"
-	],
-	"cuts": {"time.year": "2011|time.year:2012"}
+	"drilldowns": drilldowns,
+	"cuts": cuts
     };
+
     var context = {
 	dataset: "oakland-adopted-budget-fy-2011-13-expenditures",
 	siteUrl: "http://openspending.org",
-	embed: true
-    };
+        drilldown: function(node) { // Gets called on node click
+	    // If the node has children we can drill more
+	    if (node.data.node.children.length) {
+		// We create a new location by adding a url parameter
+		// The we have to check if we need to add ? or &
+		// (it depends on if there are any url parameters present).
+		// The url parameter is of the form dimension=name
+		var new_location = [window.location.href,
+				    window.location.search ? '&' : '?',
+				    drilldowns[0], '=', node.name];
+		// Go to the new location
+		window.location.href = new_location.join('');
+	    }
+	    // If the node doesn't have children we notify the user
+	    else 
+	    { 
+		// This can be made more beautiful
+		alert("Sorry, we can't dig deeper");
+	    }
+	}};
+    // Create the Treemap
     window.wdg_widget = new OpenSpending.Treemap($('#treewidget11-13'), context, state);	
-});	
+});
