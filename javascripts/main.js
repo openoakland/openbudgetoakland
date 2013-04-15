15599f61f2e863446fdb109600fb00b6e1e76b40
Tryggvi Bjorgvinsson <tryggvi.bjorgvinsson@okfn.org>
2013-04-15 22:56:07 +0000

    Added breadcrumbs below the treemap (computed based on the url parameters and drilldowns)

--- javascripts/main.js
+++ javascripts/main.js
@@ -6,13 +6,34 @@ $(function() {
     // Get url parameters (this could be easily parsed, but we use purl)
     // Purl is available here: https://github.com/allmarkedup/jQuery-URL-Parser
     var parameters = $.url().param();
-    
+
+    // Start collecting breadcrumbs. We begin with Departments (base url)
+    var path = $.url().attr('path');
+    var crumbs = [{path:path, title:'Departments'}];
+
     // While the first drilldown is in the url parameters
     // we move it to the cuts instead
     while(drilldowns[0] in parameters) {
 	var drill = drilldowns.shift();
 	cuts[drill] = parameters[drill];
+	// Add crumb to our crumbs.
+	// The path is computed from the preceeding crumb but we add
+	// the new url parameter for this particular crumb
+	crumbs.push({path:[crumbs[crumbs.length-1].path, 
+			   (crumbs.length > 1) ? '&' : '?',
+			   drill, '=', parameters[drill]].join(''),
+		     title:parameters[drill]
+		    });
     }
+
+    // Create the links for our crumbs
+    var breadcrumbs = [];
+    for (var idx in crumbs) {
+	breadcrumbs.push('<a href="'+crumbs[idx].path+'">'+
+			 crumbs[idx].title+'</a>');
+    }
+    // Add our breadcrumbs to the page (to an element with id #breadcrumbs
+    $('#breadcrumbs').html(breadcrumbs.join(' > '));
 
     // Create the state from the (possibly modified) drilldowns and cuts
     var state = {
