ba91ba1e30632f28d6a23aafcb7bea4e8871d05d
Tryggvi Bjorgvinsson <tryggvi.bjorgvinsson@okfn.org>
2013-04-15 21:42:00 +0000

    Moved the javascript code to main javascript file (could be moved into specific file). Updated oak11-13test accordingly.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,16 @@
-console.log('This would be the main JS file.');
+$(function() {
+    var state = {
+	"drilldowns": [
+	    "department",
+	    "unit",
+	    "child-fund"
+	],
+	"cuts": {"time.year": "2011|time.year:2012"}
+    };
+    var context = {
+	dataset: "oakland-adopted-budget-fy-2011-13-expenditures",
+	siteUrl: "http://openspending.org",
+	embed: true
+    };
+    window.wdg_widget = new OpenSpending.Treemap($('#treewidget11-13'), context, state);	
+});	
