0cfab09e4ca3d8d2513c43b69c4d81cce79cdfad
Ian MacFarland <ianmacfarland@ischool.berkeley.edu>
2015-04-14 16:32:12 -0700

    Revert ": Deploy interim new flow diagram design to staging site"
    This reverts commit 6f899491e25b76419a55572e6e951e35c9271401.

--- _publish-preview.sh
+++ _publish-preview.sh
@@ -3,11 +3,12 @@
 # Publish site changes to staging site
 
 URL="staging.openbudgetoakland.org"
+BASEDIR=$(dirname $0)
 FILE="../_staging/CNAME"
 COUNTER=$((COUNTER+1))
 
 # go to the 'source file' directory
-cd _src
+cd $BASEDIR/_src
 # compile files to 'staging' directory
 harp compile ./ ../_staging
 # update CNAME file with staging url
@@ -16,4 +17,4 @@ echo $URL > $FILE
 echo "Briefly describe your changes: "
 read MESSAGE
 # commit preview files and push to github
-cd ../_staging && git add -A && git commit -m "$TIMESTAMP: $MESSAGE" && git push
+cd $BASEDIR/_staging && git add -A && git commit -m "$TIMESTAMP: $MESSAGE" && git push
