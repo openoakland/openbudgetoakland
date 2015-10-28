4b66efd1119c03713d753eceea73e35b0cb056eb
Ian MacFarland <ianmacfarland@ischool.berkeley.edu>
2015-10-27 21:05:38 -0700

    scorched earth: delete any remaining legacy files from the old Harp version of the site

---
+++ routes/views/budget_visuals.js
@@ -0,0 +1,15 @@
+var keystone = require('keystone');
+
+exports = module.exports = function(req, res) {
+	
+	var view = new keystone.View(req, res);
+	var locals = res.locals;
+	
+	// locals.section is used to set the currently selected
+	// item in the header navigation.
+	locals.section = 'viz';
+	
+	// Render the view
+	view.render('budget_visuals');
+	
+};
