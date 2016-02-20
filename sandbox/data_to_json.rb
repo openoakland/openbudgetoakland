1945af0487ecd6783f945dabeb7672bbbd613a23
scott <sxm@Scotts-MacBook-Pro-2.local>
2016-02-20 09:03:41 -0600

    Create sandbox for researching potential options
    I've been working on some new d3 charts away from the structure of this
    app. I'm moving that directory into a `sandbox` folder so the work can
    be better followed and to allow others to toy with the charts as well.
    
    Currently I've duplicated a lot of our data so I can make breaking
    changes and not worry about our 'production' versions. I've also started
    on a simplified set of data because I'm so unfamiliar with our state
    data that it's hard for me to notice issues.

---
+++ sandbox/data_to_json.rb
@@ -0,0 +1,58 @@
+require 'csv'
+require 'json'
+
+puts "Please supply the name of the file"
+
+file_name = gets.strip
+csv_file_name = "#{file_name}.csv"
+
+csv = CSV.read(csv_file_name, headers: true)
+headers = csv.headers
+
+p headers
+
+puts "Please select the number corresponding to the account:"
+headers.each_with_index { |header, index| puts "#{index}: #{header}"}
+
+account = gets
+
+puts "Please select the number corresponding to the fund:"
+headers.each_with_index { |header, index| puts "#{index}: #{header}"}
+
+fund = gets
+
+puts "Please select the number corresponding to the operating unit:"
+headers.each_with_index { |header, index| puts "#{index}: #{header}"}
+
+operating_unit = gets
+
+puts "Please select the number corresponding to the operating lob:"
+headers.each_with_index { |header, index| puts "#{index}: #{header}"}
+
+lob = gets
+
+puts "Please select the number corresponding to the program name:"
+headers.each_with_index { |header, index| puts "#{index}: #{header}"}
+
+program_name = gets
+
+puts "Please select the number corresponding to the amount:"
+headers.each_with_index { |header, index| puts "#{index}: #{header}"}
+
+amount = gets
+
+data = []
+stuff = []
+csv.each do |row|
+  data << {
+    account: row[headers[account.to_i]], #row["Account Description"],
+    fund: row[headers[fund.to_i]], #row["FundDescription"],
+    unit: row[headers[operating_unit.to_i]],  #row["OperatingUnitDescription"],
+    lob: row[headers[lob.to_i]],  #row["OperatingUnitDescription"],
+    key: row[headers[program_name.to_i]], #row["ProgramName"],
+    value: row[headers[amount.to_i]] #row["Budget"].to_i
+  }
+end
+
+file = File.new("#{file_name}.json" , 'w')
+file.write data.to_json
