2fa68f8360f92090a2e6697293784b524337c219
Erik Pukinskis <erik@snowedin.net>
2016-07-26 19:36:29 -0700

    Ditch express-layout and just render the view and layout manually and patch them together

--- express-server.js
+++ express-server.js
@@ -2,31 +2,30 @@ var express = require('express');
 var app = express();
 var sassMiddleware = require('node-sass-middleware');
 var path = require('path');
-var layout = require('express-layout')
 
 app.set('view engine', 'jade');
 
 app.set('views', __dirname + '/_src');
 
-app.use(sassMiddleware({
-    /* Options */
+app.use(
+  sassMiddleware({
     src: path.join(__dirname, '_src/css'),
     dest: path.join(__dirname, 'tmp/css'),
     includePaths: path.join(__dirname, '_src'),
     debug: true,
     outputStyle: 'compressed',
     prefix:  '/css',
-    template_location:  path.join(__dirname, '_src/css')
-}));
+    template_location:  path.join(__dirname, '_src/css'),
+    force: true
+  })
+);
 
 app.use('/images', express.static('_src/images'));
 
-app.use(layout())
-
-app.set('layout', '_layout');
-
 app.get('/', function (req, res) {
-  res.render('index');
+  res.render('index', function(err, str) {
+    res.render('_layout', {yield: str});
+  });
 });
 
 app.listen(3000, function () {
