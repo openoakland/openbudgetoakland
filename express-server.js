fdda2c7182a88f474f5eaa32d7a41353e8320333
Erik Pukinskis <erik@snowedin.net>
2016-07-26 19:51:19 -0700

    Add the other pages besides index

--- express-server.js
+++ express-server.js
@@ -22,11 +22,49 @@ app.use(
 
 app.use('/images', express.static('_src/images'));
 
-app.get('/', function (req, res) {
-  res.render('index', function(err, str) {
+var pageSlugs = [
+  'index',
+  'what-we-do',
+  'who-we-are',
+  'news',
+  'contact',
+  'feedback',
+  'budget-visuals',
+  'city-of-oakland-budget',
+  'oakland-budget-101',
+  'tools-projects',
+  'budget-process',
+  '2011-2013-adopted-budget',
+  '2012-2013-sankey',
+  '2013-15-adopted-budget-landing',
+  '2013-2015-adopted-budget-flow',
+  '2013-2015-adopted-budget',
+  '2015-17-proposed-budget-flow',
+  '2015-17-proposed-budget-tree',
+  'discuss',
+  'mayor-2013-2015-proposed',
+  'public-2013-2015',
+  'updated-mayor-2013-2015-proposed'
+]  
+
+function renderJade(name, res) {
+  if (pageSlugs.indexOf(name) == -1) {
+    throw new Error("\""+name+"\" is not a page we know about.")
+  }
+
+  res.render(name, function(err, str) {
     res.render('_layout', {yield: str});
   });
+}
+
+app.get('/', function(req, res) {
+  renderJade('index', res)
 });
+
+app.get('/:slug.html', function(req, res) {
+  console.log("SLUG!", req.params.slug)
+  renderJade(req.params.slug, res)
+})
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
