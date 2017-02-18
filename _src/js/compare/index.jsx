00629ecb7d07ad9b52c27085636df0d36d9bb7d1
Ian MacFarland <ian.macfarland@gmail.com>
2017-02-18 13:41:02 -0800

    stub out new Diff page

--- _src/js/compare/index.jsx
+++ _src/js/compare/index.jsx
@@ -8,7 +8,67 @@ class Compare extends React.Component {
 
   render() {
     return <div>
-      <h2>diff</h2>
+      <h2>Compare <strong>Budget 2</strong> to <strong>Budget 1</strong></h2>
+      <Total></Total>
+      <Depts></Depts>
+      <Categories></Categories>
+    </div>
+  }
+}
+
+class Total extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div>
+      <h3>total</h3>
+      <p>+$97.7M</p>
+    </div>
+  }
+}
+
+class Depts extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const deptList = [10, 8, 7.5, 4, 2, 1].map((v, i) => {
+      return <div>
+        <dt>Department {i}</dt>
+        <dd>+{v}</dd>
+      </div>
+    })
+
+    return <div>
+      <h3>departments</h3>
+      <dl>
+        {deptList}
+      </dl>
+    </div>
+  }
+}
+
+class Categories extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const categoryList = [10, 8, 7.5, 4, 2, 1].map((v, i) => {
+      return <div>
+        <dt>Category {i}</dt>
+        <dd>+{v}</dd>
+      </div>
+    })
+
+    return <div>
+      <h3>categories</h3>
+      <dl>
+        {categoryList}
+      </dl>
     </div>
   }
 }
