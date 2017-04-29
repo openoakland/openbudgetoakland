d127870088ae138811d09d80525e26b460a54304
Ian MacFarland <ian.macfarland@gmail.com>
2017-04-29 16:43:39 -0700

    add yarn build to the staging release script

--- _publish-preview.sh
+++ _publish-preview.sh
@@ -9,8 +9,13 @@ COUNTER=$((COUNTER+1))
 
 # go to the 'source file' directory
 cd _src
+# build webpack bundles
+yarn run build
 # compile files to 'staging' directory
+# harp needs to ignore node stuff
+mv -f node_modules _node_modules
 harp compile ./ ../_staging
+mv -f _node_modules node_modules
 # update CNAME file with staging url
 echo $URL > $FILE
 # ask for description of commit
