2da434d711335bb1bf52545a3a1ad9e6fedfa1b8
nydame <nydame@users.noreply.github.com>
2021-05-09 19:57:19 +0000

    Deploying to gh-pages from  @ 47fa06455e1b898294a484c6639b69191aff2d8a 🚀

--- js/budget-glossary.js
+++ js/budget-glossary.js
@@ -33,42 +33,6 @@ function fetchDefinitions(url, options={}) {
   .catch(logError);
 }
 
-// function prepareToAppendDefinitions() {
-//   const chart = document.querySelector("#chart svg");
-//   chart.addEventListener('click', (ev) => {
-//     if (isValidTarget(ev)) appendDefinitionOnce(ev);
-//   });
-  
-// }
-
-// function isValidTarget(ev) {
-//   return (ev.target.nodeName === "rect" && ev.target.nextElementSibling.nodeName === "text");
-// }
-
-// function appendDefinitionOnce(ev) {
-//   const mainText = ev.target.nextElementSibling;
-//   const term = mainText.textContent.split(":")[0];
-//   const def = window.localStorage.getItem(term);
-
-//   if ( def && (! mainText.nextElementSibling || mainText.nextElementSibling.nodeName !== 'text') ) {
-//     const bkgColor = ev.target.style.fill;
-//     console.log(`${term}: ${def} in ${bkgColor}`);
-//     const auxiliaryText = document.createElement('text');
-//     // auxiliaryText.classList.add('term-definition');
-//     auxiliaryText.textContent = `${def}`;
-//     auxiliaryText.setAttribute('style', `fill: ${bkgColor}`);
-//     auxiliaryText.setAttribute('x', mainText.getAttribute('x'));
-//     auxiliaryText.setAttribute('dy', mainText.getAttribute('dy'));
-//     auxiliaryText.setAttribute('text-anchor', mainText.getAttribute('text-anchor'));
-//     // debugger;
-//     auxiliaryText.setAttribute('y', parseInt(mainText.getAttribute('y')) * 2);
-//     // auxiliaryText.style('background-color', bkgColor);
-//     ev.target.parentNode.appendChild(auxiliaryText);
-//   } else {
-//     console.warn(`This term is missing from the API: ${term}. Please ask OBO staff to check for misspellings or missing information in their spreadsheet.`);
-//   }
-// }
-
 fetchDefinitions( 'https://oboterms-g55nbvma6a-wn.a.run.app/Overview' );
 
 // prepareToAppendDefinitions();
