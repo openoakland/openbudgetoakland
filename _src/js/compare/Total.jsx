a782d1ffafbd5786d09f8a567f2542d0a2524c38
CodeFreq <codefreq@Macintosh-256.local>
2021-06-02 06:27:39 -0700

    Temporarily remove references to NPM package react-spinkit.

--- _src/js/compare/Total.jsx
+++ _src/js/compare/Total.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {HorizontalBar} from 'react-chartjs-2';
 import {entries} from 'd3-collection';
 import {asTick, asDiff, DiffStyled, compareChartOptions} from './utils';
-import Spinner from 'react-spinkit';
+// import Spinner from 'react-spinkit';
 
 export default class Total extends React.Component {
   constructor(props) {
@@ -11,9 +11,9 @@ export default class Total extends React.Component {
 
   render() {
     const totals = this.props.data;
-    if (!totals.length || totals.some(record => !record)) {
-      return <Spinner spinnerName="wave" noFadeIn/>
-    }
+    // if (!totals.length || totals.some(record => !record)) {
+    //   return <Spinner spinnerName="wave" noFadeIn/>
+    // }
 
     let diff = totals[0].total - totals[1].total;
     if (this.props.usePct) {
